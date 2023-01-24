4442c24f688028752d9315f556dc2574d08d5e2a
melakualehegn <melakualehegn34@gmail.com>
2023-01-24 23:17:12 +0300

    Learning fix

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
-import _ from "lodash"
+import _ from "lodash";
+
 $("body").append("<p>Holberton Dashboard<p/>");
 $("body").append("<p>Dashboard data for the students<p/>");
 $("body").append("<button>Click here to get started<button/>");
