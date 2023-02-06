96b9b82c5d073eb0fd8c7c17001dd416ee7c44a7
unknown <melakualehegn34@gmail.com>
2023-02-06 15:41:51 -0800

    deploy to github pages

---
+++ 0x01-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,26 @@
+import React from "react";
+import closeIcon from "./close-icon.png";
+import "./Notifications.css"
+import { getLatestNotification } from './utils'
+
+export function Notifications() {
+    function hundleClick() {
+        console.log("Close button has been clicked")
+    }
+    return (
+        <div className="Notifications">
+            <div>
+                <p>Here is the list of notifications</p>
+                <ul>
+                    <li data-priority="default">New course available</li>
+                    <li data-priority="urgent">New resume available</li>
+                    <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+                </ul>
+            </div>
+            <button onClick={hundleClick} style={{ background: "none", border: "none", fontSize: "18px", cursor: "pointer" }}>
+                <img src={closeIcon} width="16px"></img>
+            </button>
+        </div>
+    )
+
+}
